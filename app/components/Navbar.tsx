43710537bdf22bc96073767f7784569305e27d39
itsTari <tariebisamuel16@gmail.com>
2025-06-18 12:21:12 +0100

    set up authentication with nextAuth

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,16 +1,50 @@
-import Link from 'next/link'
-import React from 'react'
+import Link from "next/link";
+import React from "react";
+import { auth, signOut, signIn } from "@/auth";
 
-const Navbar = () => {
+
+const Navbar = async () => {
+  const session = await auth();
   return (
-    <div className='px-5 py-3 bg-white shadow-sm -font-work-sans'>
-        <nav className='flex justify-between items-center'>
-            <Link href='/'>
-                <img src='/logo.png' alt='logo' width={144} height={30}/>
-            </Link>
-        </nav>
+    <div className="px-5 py-3 bg-white text-black shadow-sm -font-work-sans">
+      <nav className="flex justify-between items-center">
+        <Link href="/">
+          <img src="/logo.png" alt="logo" width={144} height={30} />
+        </Link>
+        <div className="flex items-center gap-5">
+          {session && session?.user ? (
+            <>
+              <Link href="/startup/create">
+                <span>Create</span>
+              </Link>
+              <button
+                onClick={async () => {
+                  "use server";
+                  await signOut({ redirectTo: "/" });
+                }}
+              >
+                <span>LogOut</span>
+              </button>
+              <Link href={`/users/${session?.user?.id}`}>
+                <span>{session?.user?.name}</span>
+              </Link>
+            </>
+          ) : (
+            <>
+              <button
+                onClick={async () => {
+                  "use server";
+                  await signIn("github");
+                }}
+              >
+                <span>LogIn</span>
+              </button>
+            </>
+          )}
+        </div>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
