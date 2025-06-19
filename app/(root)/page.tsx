ab19ec332df2cf8c7f769aa95efea313554c1b0f
itsTari <tariebisamuel16@gmail.com>
2025-06-19 18:28:13 +0100

    form component for search logic

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,13 @@
-import SearchForm from "../components/SearchForm";
+import SearchForm from "../../components/SearchForm";
 
-export default function Home() {
-
+export default  async function Home({searchParams}:{searchParams: Promise<{query?: string}>}) {
+      const query = (await searchParams).query
   return (
     <>
       <section className="w-full bg-[#EE2B69] min-h-[530px] pattern flex justify-center items-center flex-col py-10 px-6;">
         <h1 className="uppercase bg-black px-6 py-3 font-work-sans font-extrabold text-white sm:text-[54px] sm:leading-[64px] text-[36px] leading-[46px] max-w-5xl text-center my-5;">Pitch Your Startup, <br/> Connect With Enterpreneurs</h1>
         <p className="font-medium text-[20px] text-white max-w-3xl text-center break-words"> Submit ideas, vote on pitches and get noticed in virtual competitions</p>
-        <SearchForm/>
+        <SearchForm  query={query}/>
       </section>
     </>
   );
--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import Form from 'next/form'
 import SearchFormReset from './SearchFormReset'
+import { Search } from 'lucide-react'
 
-const SearchForm = () => {
-    const query = 'test'
+const SearchForm = ({query}:{query?:string}) => {
+    // const query = 'test'
   
   return (
     <Form action='/' scroll={false} className='search-form max-w-3xl w-full min-h-[80px] bg-white border-[5px] border-black rounded-[80px] text-[24px] mt-8 px-5 flex flex-row items-center gap-5;'>
         <input name='query' defaultValue={query} placeholder='Search Startups' className='flex-1 font-bold placeholder:font-semibold placeholder:text-black-100 w-full h-auto outline-none;'/>
         <div className='flex gap-2'>
-            {query && (<SearchFormReset/>)}
+            {query && <SearchFormReset/>}
+            <button type='submit' className='size-[50px] rounded-full bg-black flex justify-center items-center !important text-white'><Search className='size-5'/></button>
         </div>
     </Form>
   )
