48a28c60975817a21c1c442b14c53649612fe890
itsTari <tariebisamuel16@gmail.com>
2025-06-19 15:30:41 +0100

    heading section

---
+++ app/components/SearchFormReset.tsx
@@ -0,0 +1,16 @@
+'use client'
+import React from 'react'
+
+const SearchFormReset = () => {
+    const reset = ()=>{
+        const form = document.querySelector('.search-form') as HTMLFormElement
+        if(form) form.reset()   
+    }
+  return (
+    <button type='reset' onClick={reset}>
+    
+    </button>
+  )
+}
+
+export default SearchFormReset
