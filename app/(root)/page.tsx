17141696fa9efbc908173795df5bd2b6888dab55
itsTari <tariebisamuel16@gmail.com>
2025-06-21 17:55:07 +0100

    create schema for authors and startups

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,22 +1,26 @@
 import StartupCard from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
+import { client } from "@/sanity/lib/client";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
 export default  async function Home({searchParams}:{searchParams: Promise<{query?: string}>}) {
       const query = (await searchParams).query
-
+      // fetching post
+      const posts = await client.fetch(STARTUPS_QUERY);
+      console.log(JSON.stringify(posts, null, 2))
       // dummy post
-      const posts = [
-        {
-          _createdAt: new Date(),
-          views:55,
-          author:{_id:1, name: 'Glory Samuel'},
-          _id:1,
-          description:'This is a description',
-          image:'https://t3.ftcdn.net/jpg/09/84/48/08/240_F_984480896_M8qTvRmYP94cVsgMZY9zx0pXolTqN0Ok.jpg',
-          category:'Robots',
-          title:'We Robots'
-        }
-      ]
+      // const posts = [
+      //   {
+      //     _createdAt: new Date(),
+      //     views:55,
+      //     author:{_id:1, name: 'Glory Samuel'},
+      //     _id:1,
+      //     description:'This is a description',
+      //     image:'https://t3.ftcdn.net/jpg/09/84/48/08/240_F_984480896_M8qTvRmYP94cVsgMZY9zx0pXolTqN0Ok.jpg',
+      //     category:'Robots',
+      //     title:'We Robots'
+      //   }
+      // ]
   return (
     <>
       <section className="w-full bg-[#EE2B69] min-h-[530px] pattern flex justify-center items-center flex-col py-10 px-6;">
