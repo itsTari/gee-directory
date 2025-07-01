fab514ce55d277e2832a18f86c8d226450f693bd
itsTari <tariebisamuel16@gmail.com>
2025-07-01 11:59:30 +0100

    handling parallel request

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,26 +1,28 @@
 import StartupCard, { startupTypeCard }  from "@/components/StartupCard";
 import SearchForm from "../../components/SearchForm";
-import { STARTUPS_QUERY } from "@/sanity/lib/queries";
+import { PLAYLIST_BY_SLUG_QUERY, STARTUPS_QUERY } from "@/sanity/lib/queries";
 import { sanityFetch, SanityLive } from "@/sanity/lib/live";
 import { auth } from "@/auth";
+import { client } from "@/sanity/lib/client";
 
 export default  async function Home({searchParams}:{searchParams: Promise<{query?: string}>}) {
       const query = (await searchParams).query
       // search input field
       const params = {search: query || null}
-
       // extracting a session from the home page 
       const session = await auth()
-      // if (!session) {
-      // // Handle unauthenticated state
-      // return <div>Please sign in</div>
-      // }
-      console.log(session?.id)
+      // console.log(session?.id)
 
       // fetching post
       // const posts = await client.fetch(STARTUPS_QUERY);
-      const {data: posts} = await sanityFetch({query :STARTUPS_QUERY, params})
-      // console.log(JSON.stringify(posts, null, 2))
+      // const {data: posts} = await sanityFetch({query :STARTUPS_QUERY, params}) 
+      // for the category fetch, to get startups by catgory
+      // const {select: editorPosts} = await client.fetch( PLAYLIST_BY_SLUG_QUERY, {slug:'editor-picks'})
+    // handling multiple request using parallel request
+    const [{data: posts},  {select: editorPosts} ] = await Promise.all([
+      sanityFetch({query :STARTUPS_QUERY, params}),
+      client.fetch( PLAYLIST_BY_SLUG_QUERY, {slug:'editor-picks'})
+    ])
       
       
   return (
@@ -40,6 +42,19 @@ export default  async function Home({searchParams}:{searchParams: Promise<{query
             }
         </ul>
       </section>
+
+      {editorPosts?.length > 0 && (
+        <div className="max-w-4xl mx-auto">
+          <p className="text-30-semibold">Editors picks</p>
+          <p>What editors find interesting the most, their most visited startups</p>
+          <ul className="mt-10 mx-2 grid sm:grid-cols-2 gap-5">
+            {editorPosts.map((post : startupTypeCard, id:number)=>
+              <StartupCard post={post} key={id}/>
+            )}
+          </ul>
+        </div>
+      )}
+
       <SanityLive/>
     </>
   );
